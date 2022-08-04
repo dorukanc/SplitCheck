f7dd0de2db6d74e9786535523083992fcc89370d
dorukanc <catakd@mef.edu.tr>
2022-08-04 19:25:26 +0300

    Initial Commit

---
+++ Shared/SplitCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitCheckApp.swift
+//  Shared
+//
+//  Created by Dorukan Çatak on 4.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SplitCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
